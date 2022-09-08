11d4af5146ad6f22169456513a1152109135712d
Tomek-Zuczek <zaktomek14@gmail.com>
2022-09-08 11:01:40 +0200

    Added page

---
+++ strona/druga.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+/*
+$a = 1;
+$b = 3;
+
+echo $a + $b;
+echo ($b -$a) * ($b - $a);
+*/
+
+
+$a = 0;
+switch($a){
+    case " ":
+        echo "Spacja";
+        break;
+    
+    case 5:
+        echo "a to 5";
+        break;
+
+    case 0:
+        echo "a to 0";
+        break;
+
+    default:
+    echo "co jest xd";
+    break;
+}
+/*
+$zm = 3;
+
+if($zm%2==0)
+    {
+    echo "liczba jest parzysta";
+    
+    }
+    else
+    {
+        echo "liczba jest nieparzysta";
+    }
+
+
+
+
+*/
+?>
+
+</body>
+</html>
