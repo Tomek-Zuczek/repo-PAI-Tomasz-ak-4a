2bc1be69470cbcc25092fbe9b01ac781a956ab9d
Tomasz Żak <zaktomek14@gmail.com>
2022-09-12 12:43:43 +0200

    Update repo

---
+++ php 12.0.2022/main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+
+<body>
+    <?php
+    $mycounter = 1;
+
+
+    echo "Hello World<br>";
+    echo "My counter<br>".$mycounter;
+    echo "Hello friend<br>";
+
+
+    ?>
+</body>
+
+</html>
---
+++ php 12.0.2022/pehap.php
@@ -0,0 +1,20 @@
+<html>
+    
+</html>
+
+<?php
+    //komentarz
+    /*Taki długi komentarz*/
+
+    $tekst = "Hello Friend";
+
+    echo "Hello World"
+    echo "My counter 1";
+    echo $tekst;
+
+
+    
+    
+
+
+?>
