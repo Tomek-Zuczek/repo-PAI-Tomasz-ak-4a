833f69ce788617945b716f0b02ae0065313406f7
Tomasz Żak <zaktomek14@gmail.com>
2022-09-21 11:52:10 +0200

    IF I BREAK

---
+++ if i break-continte/break.php
@@ -0,0 +1,11 @@
+<?php
+    
+    for($j=-3;$j<=3;$j++)
+    {
+        if($j==0)
+        {
+            continue;
+        }
+        echo $j ."<br>";
+    }
+?>
---
+++ if i break-continte/if procenty.php
@@ -0,0 +1,30 @@
+<?php
+
+    $wynik = 90;
+
+    if($wynik<=39)
+    {
+        echo "Dostales 1";
+    }
+    else if($wynik>=40 && $wynik<=54)
+    {
+        echo "Dostales 2";
+    }
+    else if($wynik>=55 && $wynik<=69)
+    {
+        echo "Dostales 3";
+    }
+    else if($wynik>=70 && $wynik<=84)
+    {
+        echo "Dostales 4";
+    }
+    else if($wynik>=85 && $wynik<=94)
+    {
+        echo "Dostales 5";
+    }
+    else if($wynik>=95 && $wynik<=100)
+    {
+        echo "Dostales 6";
+    }
+    
+?>
