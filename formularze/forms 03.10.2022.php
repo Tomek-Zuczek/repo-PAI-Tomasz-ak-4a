feffbf815a0f0643eec4b0af2810b1940e3adadc
Tomek-Zuczek <zaktomek14@gmail.com>
2022-10-12 11:58:22 +0200

    Update

--- formularze/forms 03.10.2022.php
+++ formularze/forms 03.10.2022.php
@@ -7,12 +7,41 @@
     <form action="./forms_ans.php" method="POST">
     <p>Logowanie użytkownika</p>
         <p>Login</p>
-        <p><input type="text" name="dane[]"></p>
+        <p><input type="text" name="login"></p>
         <br>
-        hasło
-        <p><input type="password" name="dane[]"></p>
+        Hasło
+        <p><input type="password" name="haslo"></p>
         <br>
-        <p><input type="checkbox" name="dane[]" value="Zapamiętaj mnie" />Zapamiętaj mnie</p>        
+        <p><input type="checkbox" name="dane" value="Zapamiętaj mnie" />Zapamiętaj mnie</p>        
         <input type="submit" />
+        <br><br><br>
+
+        <p>Pseudonim</p>
+        <input type="text" name="Pseudonim">
+        <br>
+        <p>Komentarz</p>
+        <textarea name="Komentarz"></textarea>
+        <br><br><br>
+
+        <p>Czy jesteś człowiekem?</p>
+        <select name="opcje">
+            <option>Tak</option>
+            <option>Nie</option>
+            <option>Nie wiem</option>
+        </select>
+        <br><br>
+
+        <p>Jestem uczniem na roku:</p>
+        <input type="radio" name="radio[]" value="1">1
+        <input type="radio" name="radio[]" value="2">2
+        <input type="radio" name="radio[]" value="3">3
+        <br><br>
+
+        <p>Chciałbym się nauczyć:</p>
+        <input type="checkbox" name="check[]" value="Życia"/>Życia
+        <input type="checkbox" name="check[]" value="Robienia masła"/>Robienia masła
+        <input type="checkbox" name="check[]" value="Programowania"/>Programowania
+        
+
     </form>
     </body>
--- formularze/forms_ans.php
+++ formularze/forms_ans.php
@@ -1,10 +1,60 @@
 <?php
-        if(isset($_POST['dane'])){
-            for($i=0; $i<count($_POST['dane']); $i++){
-                echo sha1($_POST['dane'][$i] . "<br />");
-            }
-        }else{
-            echo "Brak danych";
+
+    if(isset($_POST['login'])){
+        echo 'Login: ' , $_POST['login'];
+        echo '<br>';
+    }
+
+    if(isset($_POST['haslo'])){
+        echo 'Hasło: ' , sha1($_POST['haslo']);
+        echo '<br>';
+    }
+
+    if(isset($_POST['dane'])){
+        echo 'Pole "zapamiętaj" mnie zostało zaznaczone';
+        echo '<br>';
+        echo '<br>';
+    }else{
+        echo 'Pole "zapamiętaj mnie" nie zostało zaznaczone';
+        echo '<br>';
+        echo '<br>';
+    }
+
+    if(isset($_POST['Pseudonim'])){
+        echo 'Pseudonim: ' , $_POST['Pseudonim'];
+        echo '<br>';
+    }
+
+    if(isset($_POST['Komentarz'])){
+        echo 'Komentarz: ' , $_POST['Komentarz'];
+        echo '<br>';
+        echo 'Komentarz: ' , $_POST['Komentarz'];
+        echo '<br><br>';
+    }
+
+    if(isset($_POST['opcje'])){
+        $zaznaczone = $_POST['opcje'];
+        echo 'Na pytanie odpowiedziałeś: ' , $zaznaczone;
+        echo '<br>';
+    }
+
+    if(isset($_POST['radio'])) {    
+        foreach($_POST['radio'] as $value){
+            echo "Jestem uczniem na roku: ".$value.'<br/>';
         }
-        
+    }
+    
+  
+
+    if(isset($_POST['check'])) {    
+        foreach($_POST['check'] as $value){
+            echo "Chciałbym sie nauczyć: ".$value.'<br/>';
+        }
+    }
+    
+    
+    
+
+
+
 ?>
