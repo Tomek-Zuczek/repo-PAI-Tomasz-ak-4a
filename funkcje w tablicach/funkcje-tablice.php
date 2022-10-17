983f4faaa764f5d6b7db8d1e14438cfb08ea72e9
Tomasz Żak <zaktomek14@gmail.com>
2022-10-17 11:02:48 +0200

    tablice

---
+++ funkcje w tablicach/funkcje-tablice.php
@@ -0,0 +1,41 @@
+<?php
+    $liczby = array("14", "27", "39", "45", "69", "4", "28", "99", "1", "10");
+    $zdanie = "Bardzo lubię jeść dobre jedzenie";
+    $temp = explode(' ',$zdanie);
+    print_r ($liczby) ;
+    echo '<br>';
+    print_r ($temp);
+    echo '<br>';
+
+
+    $str = explode(' ', "Rano spóźniłem się na autobus");
+
+    echo '<br>' ,'Ilość elementów tablicy 1: ' , count($liczby);
+    echo '<br>' ,'Ilość elementów tablicy 2: ' , count($str);
+    echo '<br><br>';
+
+    sort($liczby, SORT_NUMERIC);
+    sort($str, SORT_STRING);
+    echo 'Sortowanie numeryczne: ';
+    print_r ($liczby);
+    echo '<br>';
+    echo 'Sortowanie znakowe: ';
+    print_r ($str);
+    echo '<br><br>';
+
+    shuffle($liczby);
+    shuffle($str);
+    print_r ($liczby);
+    echo '<br>';
+    print_r ($str);
+    echo '<br><br>';
+
+    
+    echo current($liczby) . '<br>';
+    next($liczby);
+    next($liczby);
+    echo current($liczby) . '<br>';
+
+    reset($liczby);
+    echo current($liczby);
+?>
