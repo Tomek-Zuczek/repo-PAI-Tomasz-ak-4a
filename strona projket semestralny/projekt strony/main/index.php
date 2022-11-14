0e8672ebfdaa5388f2a9e204be27afbfa7d72451
Tomasz Żak <zaktomek14@gmail.com>
2022-11-14 09:15:31 +0100

    update

---
+++ strona projket semestralny/projekt strony/main/index.php
@@ -0,0 +1,77 @@
+<DOCTYPE html!>
+<html>
+    <head>
+        <meta charset="utf=8">
+	    <link rel="stylesheet" href="style.css">
+	    <title> Smaczny Szczecin </title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@500&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+
+    <nav>
+    </nav>
+
+    <header>
+        <section id="backsection">
+            <div id="background">
+                <section id="buttons">
+                    <p id="rev">Napisz recenzję</p>
+                    <p id="login">Zaloguj się</p>
+                    <p id="signin">Zarejestruj się</p>
+                </section>
+                <section id="abc">
+                    <img id="smacznelogo" src="img/smaczny2.png">
+                    <p id="banertext">Znajdź najlepsze smaki Szczecina</p>
+                    <form>
+                        <input type="text" id="searchbar" placeholder="Wyszukaj bo nazwie restauracji lub potrawy">
+                    </form>
+                </section>
+            </div>
+        </section>
+
+    </header>
+
+    <div id="content">
+        <div id="ostatniododane">
+            Ostatnio dodane recenzje
+        </div>
+
+        <div id="reviews">
+            <div class="review">
+                <form  type="textarea" id="text1">
+                </form>
+            </div>
+
+            <div class="review">
+            </div>
+
+            <div class="review">
+            </div>
+        </div>
+
+
+             
+    </div>
+
+
+    <footer>
+        <section id="info">
+            <span id="about">O nas</span>
+            <span id="social">Social Media</span>
+            <span id="lang">Język</span>
+        </section>
+    </footer>
+
+
+    </body>
+
+
+
+
+
+
+
+</html>
